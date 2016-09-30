d1ccbe8f7d18ac97153b4f86215ad3cbb759c6e8
thechucklingatom <thechucklingatom@gmail.com>
2016-09-30 15:23:16 -0600

    formatting

--- src/Project2/CSCI446/Main.java
+++ src/Project2/CSCI446/Main.java
@@ -5,7 +5,7 @@ package Project2.CSCI446;
  */
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+	public static void main(String[] args) {
+		// write your code here
+	}
 }
