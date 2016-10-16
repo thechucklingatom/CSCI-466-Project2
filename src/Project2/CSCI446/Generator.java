b4cdf0df9b11edba32c07c2eda5a66964e1dbc99
MathewGostnell <mathew.gostnell@gmail.com>
2016-10-16 16:47:16 -0600

    Added Room and Generator

---
+++ src/Project2/CSCI446/Generator.java
@@ -0,0 +1,151 @@
+package Project2.CSCI446;
+
+import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Created by Thew on 10/16/2016.
+ */
+public class Generator {
+    private Room[][] world;
+    private Random rng;
+    private int worldSize;
+    private double pitProb;
+    private double wumpusProb;
+    private double obstacleProb;
+    private int startPoint;
+
+    public Generator(int worldSize, double pitProb, double wumpusProb, double obstacleProb) {
+        this.worldSize = worldSize;
+        this.pitProb = pitProb;
+        this.wumpusProb = wumpusProb;
+        this.obstacleProb = obstacleProb;
+        this.rng = new Random();
+        world = new Room[worldSize][worldSize];
+        startPoint = -1;
+
+        this.GenerateWorld();
+    }
+
+    private void GenerateWorld() {
+        for (int row = 0; row < worldSize; row++) {
+            for (int column = 0; column < worldSize; column++) {
+                double rnd = rng.nextFloat();
+                // randomly add possible RoomTypes based on probability
+                List<RoomType> roomRoles = new ArrayList<RoomType>();
+                if (rnd < pitProb) {
+                    roomRoles.add(RoomType.PIT);
+                }
+                if (rnd < wumpusProb) {
+                    roomRoles.add(RoomType.WUMPUS);
+                }
+                if (rnd < obstacleProb) {
+                    roomRoles.add(RoomType.OBSTACLE);
+                }
+                // add an empty room by default
+                roomRoles.add(RoomType.EMPTY);
+
+                // update current room with randomly selected RoomType and current position
+                int selection = rng.nextInt(roomRoles.size());
+                RoomType selRoom = roomRoles.get(selection);
+                world[row][column].setType(selRoom);
+                world[row][column].setXPosition(row);
+                world[row][column].setYPosition(column);
+
+                // update percepts in adj. Rooms
+                switch (selRoom) {
+                    case PIT:
+                        updatePercepts(row, column, Percept.WINDY);
+                        break;
+
+                    case OBSTACLE:
+                        updatePercepts(row, column, Percept.BUMP);
+                        break;
+
+                    case WUMPUS:
+                        updatePercepts(row, column, Percept.SMELLY);
+                        break;
+                }
+            }
+        }
+
+        // randomly select a point to house the Gold if it is empty
+        RoomType currentType;
+        int xPos;
+        int yPos;
+        do {
+            xPos = rng.nextInt(worldSize);
+            yPos = rng.nextInt(worldSize);
+            currentType = world[xPos][yPos].getType();
+        } while (currentType != RoomType.EMPTY);
+
+        // set goal state
+        world[xPos][yPos].setType(RoomType.GOLD);
+
+        // randomly select a start point for the Explorer
+        do {
+            xPos = rng.nextInt(worldSize);
+            yPos = rng.nextInt(worldSize);
+            currentType = world[xPos][yPos].getType();
+        } while (currentType != RoomType.EMPTY && !hasValidPath(xPos, yPos));
+
+        // set start point as an integer
+        startPoint = xPos * (worldSize + 1) + yPos;
+    }
+
+    public int startXPosition() {
+        return (int) startPoint / (worldSize + 1);
+    }
+
+    public int startYPosition() {
+        return startPoint % (worldSize + 1);
+    }
+
+    public boolean hasValidPath(int x, int y) {
+        if (world[x][y].getType() == RoomType.GOLD) {
+            return true;
+        } else {
+            boolean lRoom = false, rRoom = false, uRoom = false, dRoom = false;
+            if (x > 0) {
+                lRoom = hasValidPath(x-1, y);
+            }
+            if (x + 1 < worldSize) {
+                rRoom = hasValidPath(x+1, y);
+            }
+            if (y > 0) {
+                dRoom = hasValidPath(x, y-1);
+            }
+            if (y + 1 < worldSize) {
+                uRoom = hasValidPath(x, y+1);
+            }
+
+            return lRoom || rRoom || uRoom || dRoom;
+        }
+    }
+
+    public Room[][] getWorld() {
+        return world;
+    }
+
+    private void updatePercepts(int x, int y, Percept sense) {
+        if (x + 1 < worldSize) {
+            // we have adj. Room to the right of this point
+            world[x+1][y].addPercept(sense);
+        }
+        if (x > 0) {
+            // we have adj. Room to the left of this point
+            world[x-1][y].addPercept(sense);
+        }
+
+        if (y + 1 < worldSize) {
+            // we have adj. Room above this point
+            world[x][y+1].addPercept(sense);
+        }
+
+        if (y > 0) {
+            // we have adj. Room below this point
+            world[x][y-1].addPercept(sense);
+        }
+    }
+}
---
+++ src/Project2/CSCI446/Room.java
@@ -0,0 +1,95 @@
+package Project2.CSCI446;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Created by Thew on 10/16/2016.
+ */
+public class Room {
+    private RoomType type;  // the current known Type for this Room
+    private int xPosition;    // set x-position of this Room in world
+    private int yPosition;    // set y-position of this Room in world
+    private final List<RoomType> possibleTypes; // possible Types that might apply to this Room
+    private final List<Percept> percepts;   // all current percepts for this given room
+
+    public Room(int x, int y) {
+        this.xPosition = x;
+        this.yPosition = y;
+        this.type = RoomType.EMPTY;
+        possibleTypes = new ArrayList<RoomType>();
+        percepts = new ArrayList<Percept>();
+
+    }
+
+    public Room() {
+        this.xPosition = -1;
+        this.yPosition = -1;
+        this.type = RoomType.EMPTY;
+        possibleTypes = new ArrayList<RoomType>();
+        percepts = new ArrayList<Percept>();
+    }
+
+    public List<Percept> getPercepts() {
+        return this.percepts;
+    }
+
+    public void addPercept(Percept sense) {
+        this.percepts.add(sense);
+    }
+
+    public void delPercept(Percept sense) {
+        this.percepts.remove(sense);
+    }
+
+    public void setType(RoomType type) {
+        this.type = type;
+    }
+
+    public RoomType getType() {
+        return this.type;
+    }
+
+    public int getXPosition() {
+        return this.xPosition;
+    }
+
+    public int getYPosition() {
+        return this.yPosition;
+    }
+
+    public void setXPosition(int xPosition) {
+        this.xPosition = xPosition;
+    }
+
+    public void setYPosition(int yPosition) {
+        this.yPosition = yPosition;
+    }
+
+    public void addPossibleType(RoomType newType) {
+        possibleTypes.add(newType);
+    }
+
+    public void removePossibleType(RoomType delType) {
+        possibleTypes.remove(delType);
+    }
+
+    public List<RoomType> getPossibleTypes() {
+        return this.possibleTypes;
+    }
+
+    public String toString() {
+        String toString;
+        toString = "Room[" + this.xPosition + "][" + this.yPosition + "] of type: " + this.type + " or possibly: ";
+        for (RoomType pos : this.possibleTypes) {
+            toString += pos.toString() + ", ";
+        }
+        return toString;
+    }
+
+    public void print() {
+        System.out.print(toString() + "\n");
+    }
+
+
+}
