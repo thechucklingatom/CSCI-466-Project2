104a6a29db44b5311fe097063a4bc11cfd2e782d
Robert Putnam <thechucklingatom@gmail.com>
2016-10-20 21:08:20 -0600

    added csv writer

--- src/Project2/CSCI446/Main.java
+++ src/Project2/CSCI446/Main.java
@@ -1,14 +1,28 @@
 package Project2.CSCI446;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 /**
  * @author thechucklingatom
  */
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// write your code here
 
-		Generator generator = new Generator(5, .1, .1, .1);
+		int worldSize = 5;
+		double wumpusProb = .1, obstacleProb = .1, pitProb = .1;
+
+		PrintWriter writer = new PrintWriter("tests.csv");
+
+		String header = "Player Type,Total Score,World Size,Wumpus Probability,Obstacle "
+				+ "Probability,Pit Probability,Total Deaths,Pit Deaths,Wumpus Deaths,"
+				+ "Have gold";
+
+		writer.println(header);
+
+		Generator generator = new Generator(worldSize, pitProb, wumpusProb, obstacleProb);
 
 		World gameWorld = new World(generator.getWorld(), generator.startXPosition(),
 				generator.startYPosition());
@@ -17,10 +31,24 @@ public class Main {
 
 		player.solve();
 
-		System.out.println(player.totalCost);
-		for (RoomType death :
+		int wumpusDeaths = 0;
+		int pitDeaths = 0;
+
+		for (RoomType roomType :
 				player.deaths) {
-			System.out.println(death);
+			if (roomType == RoomType.WUMPUS) {
+				wumpusDeaths++;
+			} else if (roomType == RoomType.PIT) {
+				pitDeaths++;
+			}
 		}
+
+		String toWrite = String.format("%s,%d,%d,%f,%f,%f,%d,%d,%d,%b", player.getClass().getSimpleName(),
+				player.totalCost, worldSize, wumpusProb, obstacleProb, pitProb, player.deaths.size(),
+				pitDeaths, wumpusDeaths, player.haveGold);
+
+		writer.println(toWrite);
+
+		writer.close();
 	}
 }
